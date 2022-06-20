a32b634faa2737c3e29c4656eef43b14c892b2c9
Anderson Suga <anderson.suga@gmail.com>
2022-06-20 14:48:41 -0300

    Aula 12

--- sqla_sync_03/conf/db_session.py
+++ sqla_sync_03/conf/db_session.py
@@ -24,7 +24,7 @@ def create_engine(sqlite: bool = False) -> Engine:
         conn_str = f'sqlite:///{arquivo_db}'
         __engine = sa.create_engine(url=conn_str, echo=False, connect_args={'check_same_thread': False})
     else:
-        conn_str = 'postgresql://aki:asdf@localhost:5437/picoles'
+        conn_str = 'postgresql://anderson:Teste123@localhost:5432/picoles'
         __engine = sa.create_engine(url=conn_str, echo=False)
 
     return __engine
---
+++ sqla_sync_03/models/lote.py
@@ -0,0 +1,20 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from datetime import datetime
+from sqla_sync_03.models.model_base import ModelBase
+from sqla_sync_03.models.tipo_picole import TiposPicole
+
+
+class Lote(ModelBase):
+    __tablename__: str = 'lotes'
+
+    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True)
+
+    id_tipo_picole: int = sa.Column(sa.Integer, sa.ForeignKey('tipos_picole.id'))
+    tipo_picole: TiposPicole = orm.relationship('TipoPicole', lazy='joined')
+
+    quantidade: int = sa.Column(sa.Integer, nullable=False)
+
+    def __repr__(self) -> int:
+        return f'<Lote: {self.id}>'
