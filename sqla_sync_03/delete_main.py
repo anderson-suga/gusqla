67a775eb27ed528546286e027580048ec0517c1c
Anderson Suga <anderson.suga@gmail.com>
2022-06-21 15:27:21 -0300

    Aula 25

---
+++ sqla_sync_03/delete_main.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from sqla_sync_03.conf.db_session import create_session
+
+from sqla_sync_03.models.picole import Picole
+from sqla_sync_03.models.revendedor import Revendedor
+
+
+def deletar_picole(id_picole: int) -> None:
+    with create_session() as session:
+        picole: Optional[Picole] = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+
+        if picole:
+            session.delete(picole)
+            session.commit()
+        else:
+            print(f'Não encontrei picole com ID {id_picole}')
+
+
+def deletar_revendedor(id_revendedor: int) -> None:
+    with create_session() as session:
+        revendedor: Optional[Revendedor] = session.query(Revendedor).filter(
+            Revendedor.id == id_revendedor).one_or_none()
+
+        if revendedor:
+            session.delete(revendedor)
+            session.commit()
+        else:
+            print(f'Não encontrei revendedor com ID {id_revendedor}')
+
+
+if __name__ == '__main__':
+    pass
