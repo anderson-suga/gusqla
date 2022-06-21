4aa02bb27f171419302200df332f7fcbd64cda3f
Anderson Suga <anderson.suga@gmail.com>
2022-06-21 15:17:16 -0300

    Aula 24

---
+++ sqla_sync_03/update_main.py
@@ -0,0 +1,32 @@
+from sqla_sync_03.conf.db_session import create_session
+
+from sqla_sync_03.models.sabor import Sabor
+from sqla_sync_03.models.picole import Picole
+
+
+def atualiza_sabor(id_sabor: int, novo_sabor: str) -> None:
+    with create_session() as session:
+        sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).one_or_none()
+
+        if sabor:
+            sabor.nome = novo_sabor
+            session.commit()
+        else:
+            print(f'Não existe sabor com ID {id_sabor}')
+
+
+def atualiza_picole(id_picole: int, novo_preco: float, novo_sabor: int = None):
+    with create_session() as session:
+        picole: Picole = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+
+        if picole:
+            picole.preco = novo_preco
+            if novo_sabor:
+                picole.id_sabor = novo_sabor
+            session.commit()
+        else:
+            print(f'Não existe sabor com ID {id_picole}')
+
+
+if __name__ == '__main__':
+    pass
