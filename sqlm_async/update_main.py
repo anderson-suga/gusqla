88a76395266f72f278811e700a84dd6487b3b2fd
Anderson Suga <anderson.suga@gmail.com>
2022-06-23 11:29:17 -0300

    aula 58

--- sqlm_async/update_main.py
+++ sqlm_async/update_main.py
@@ -6,26 +6,26 @@ from sqlm_async.models.sabor import Sabor
 from sqlm_async.models.picole import Picole
 
 
-def atualiza_sabor(id_sabor: int, novo_sabor: str) -> None:
-    with create_session() as session:
+async def atualiza_sabor(id_sabor: int, novo_sabor: str) -> None:
+    async with create_session() as session:
         sabor: Sabor = session.get(Sabor, id_sabor)
 
         if sabor:
             sabor.nome = novo_sabor
-            session.commit()
+            await session.commit()
         else:
             print(f'Não existe sabor com ID {id_sabor}')
 
 
-def atualiza_picole(id_picole: int, novo_preco: float, novo_sabor: int = None):
-    with create_session() as session:
+async def atualiza_picole(id_picole: int, novo_preco: float, novo_sabor: int = None):
+    async with create_session() as session:
         picole: Picole = session.get(Picole, id_picole)
 
         if picole:
             picole.preco = novo_preco
             if novo_sabor:
                 picole.id_sabor = novo_sabor
-            session.commit()
+            await session.commit()
         else:
             print(f'Não existe sabor com ID {id_picole}')
 
