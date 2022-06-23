97ef4f10405c7df1bc151fa96416cd880c18b151
Anderson Suga <anderson.suga@gmail.com>
2022-06-23 10:22:10 -0300

    aula 48

--- sqlm_sync/update_main.py
+++ sqlm_sync/update_main.py
@@ -1,12 +1,14 @@
-from sqla_sync.conf.db_session import create_session
+from sqlm_sync.conf.db_session import create_session
 
-from sqla_sync.models.sabor import Sabor
-from sqla_sync.models.picole import Picole
+from sqlmodel import select
+
+from sqlm_sync.models.sabor import Sabor
+from sqlm_sync.models.picole import Picole
 
 
 def atualiza_sabor(id_sabor: int, novo_sabor: str) -> None:
     with create_session() as session:
-        sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).one_or_none()
+        sabor: Sabor = session.get(Sabor, id_sabor)
 
         if sabor:
             sabor.nome = novo_sabor
@@ -17,7 +19,7 @@ def atualiza_sabor(id_sabor: int, novo_sabor: str) -> None:
 
 def atualiza_picole(id_picole: int, novo_preco: float, novo_sabor: int = None):
     with create_session() as session:
-        picole: Picole = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+        picole: Picole = session.get(Picole, id_picole)
 
         if picole:
             picole.preco = novo_preco
